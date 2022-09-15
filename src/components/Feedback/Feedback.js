0b6c867b32da626474562548a185754972b5205e
AnastasiyaSeroshtan <97408274+AnastasiyaSeroshtan@users.noreply.github.com>
2022-09-16 00:56:05 +0300

    to do 1 path 1-th task

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -9,20 +9,44 @@ export class Feedback extends React.Component {
         bad: 0
       }
 
+     countsNumberClickGood = () => {
+        this.setState(prevState => {
+            return {
+                good: prevState.good + 1,
+            }
+        })
+     };
+
+     countsNumberClickNeutral = () => {
+        this.setState(prevState => {
+            return {
+                neutral: prevState.neutral + 1,
+            }
+        })
+     };
+
+     countsNumberClickBad = () => {
+        this.setState(prevState => {
+            return {
+                bad: prevState.bad + 1,
+            }
+        })
+     };
+
     render() {
         return (
             <Box pl={5}>
                 <FeedbackText>Please leave feedback</FeedbackText>
                 <Box mb={5}>
-                <Button type="button">Good</Button>
-                <Button type="button">Neutral</Button>
-                <Button type="button">Bad</Button>
+                <Button type="button" onClick={this.countsNumberClickGood}>Good</Button>
+                <Button type="button" onClick={this.countsNumberClickNeutral}>Neutral</Button>
+                <Button type="button" onClick={this.countsNumberClickBad}>Bad</Button>
                 </Box>
                 <StatisticsText>Statistics</StatisticsText>
                 <Box as="ul">
-                    <StatisticsItem>Good: </StatisticsItem>
-                    <StatisticsItem>Neutral: </StatisticsItem>
-                    <StatisticsItem>Bad: </StatisticsItem>
+                    <StatisticsItem>Good: {this.state.good} </StatisticsItem>
+                    <StatisticsItem>Neutral: {this.state.neutral} </StatisticsItem>
+                    <StatisticsItem>Bad: {this.state.bad} </StatisticsItem>
                 </Box>
             </Box>
         )
