f4300954bc9fa9884165b820d386ae776dff042e
AnastasiyaSeroshtan <97408274+AnastasiyaSeroshtan@users.noreply.github.com>
2022-10-12 18:31:13 +0300

    to do task

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,59 +1,76 @@
-import React from "react";
+import { useState } from "react";
 import { Box } from "../Box";
 import { Statistics } from "./Statistics/Statistics";
 import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
 import { Section} from "./Section/Section";
 import { Notification } from "./Notification/Notification";
 
-export class Feedback extends React.Component {
-    state = {
-        good: 0,
-        neutral: 0,
-        bad: 0,
-      }
+export const Feedback = () => {
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
 
-    countsNumberClick = (option) => {
-            this.setState(prevState => {
-                return {
-                    [option]: prevState[option] + 1,
-                }
-            })
+    const option = {good, neutral, bad};
+    const keys = Object.keys(option);
+
+    const countsNumberClick = (option) => {
+           if (option === keys[0]) {
+            setGood(prevState => prevState+1);
+           }
+           else if (option ===  keys[1] ) {
+            setNeutral(prevState => prevState+1)
+           }
+           else if (option === keys[2]) {
+            setBad(prevState => prevState+1)
+           }
          };
 
-     countTotalFeedback = () =>{
-        return (
-            this.state.good + this.state.neutral + this.state.bad
-        )
-     };
+        
+// Інший варіант countsNumberClick:
 
-     countPositiveFeedbackPercentage = () => {
-        return (
-            this.state.good*100/this.countTotalFeedback()
-        )
-     };
+    // const countsNumberClick = (feedBackItem) => {
+    //     switch(feedBackItem){
+    //         case 'good':
+    //          setGood(prevState => prevState+1);
+    //          break;
 
-    render() {
-        const total = this.countTotalFeedback();
-        const positeveFeedback = this.countPositiveFeedbackPercentage() ?
-        Math.ceil(this.countPositiveFeedbackPercentage())
+    //         case 'neutral':
+    //           setNeutral(prevState => prevState+1);
+    //           break;
+
+    //          case 'bad':
+    //            setBad(prevState => prevState+1);
+    //            break;
+
+    //           default:
+    //             break;      
+    //     }  
+    //   };
+
+    const countTotalFeedback = () => good + neutral + bad;
+
+    const countPositiveFeedbackPercentage = () => good*100/countTotalFeedback();
+
+        const total = countTotalFeedback();
+        const positeveFeedback = countPositiveFeedbackPercentage() ?
+        Math.ceil(countPositiveFeedbackPercentage())
         : 0;
         return (
             <Box pl={5}>
                 <Section title={"Please leave feedback"}>
                     <FeedbackOptions 
-                    options={Object.keys(this.state)}
-                    onLeaveFeedback={this.countsNumberClick}  />
+                    options={keys}
+                    onLeaveFeedback={countsNumberClick}  />
                 </Section>
                 <Section title={"Statistics"}>
                     {total > 0 ? 
-                    <Statistics good={this.state.good} 
-                    neutral={this.state.neutral} 
-                    bad={this.state.bad} 
+                    <Statistics good={good} 
+                    neutral={neutral} 
+                    bad={bad} 
                     total={total} 
                     positeveFeedback={positeveFeedback} />
                     : <Notification message={"There is no feedback"} />}
                 </Section>
             </Box>
         )
-    }
 }
--- src/components/Feedback/Notification/Notification.js
+++ src/components/Feedback/Notification/Notification.js
@@ -1,5 +1,4 @@
 import PropTypes from 'prop-types';
-
 import { NotificationText } from "./Notification.styled";
 
 export const Notification = ({message}) => {
